77c0d70285c81a83bb95b329ed55fb526c4f95d7
Kat Lasota <lasota.kat@gmail.com>
2018-08-23 19:41:09 +0200

    17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '80036905369-ravpog97q0pp812or5j0u7m8kih750hr.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'wUspO1Q2NPldA6lQpw-Js81N',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
